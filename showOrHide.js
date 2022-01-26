cd7442cb31f8af686cb715f7863d9073b20e5669
dsjb <84160996+waterstorm29@users.noreply.github.com>
2022-01-26 23:08:19 +0800

    Upload 01

---
+++ showOrHide.js
@@ -0,0 +1,15 @@
+var a = 1;
+function show_hide()
+{
+	if(a==1)
+		{
+			document.getElementById("image").style.display="inline";
+			return a=0;
+		}
+	else
+		{
+			document.getElementById("image").style.display="none";
+			return a=1;
+		}
+	
+}
